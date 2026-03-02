7c91c9d1a022c6d971f14da5a2aaa438f80e4653
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-03-03 06:15:36 +0800

    Add new weather information.

--- Contents/mods/MBGWorldInfo/42/media/lua/server/MBGWorldInfo/MBGWorldInfo.lua
+++ Contents/mods/MBGWorldInfo/42/media/lua/server/MBGWorldInfo/MBGWorldInfo.lua
@@ -4,22 +4,16 @@ print("[MBGWorldInfo] loaded")
 -- World Age
 local function getWorldAgeDays()
 	local gameTime = getGameTime()
-	
-    if not gameTime then return end
-	
 	local ageHours = gameTime:getWorldAgeHours()
 	local ageDays = ageHours / 24
 	
-	return ageDays
+	return math.floor(ageDays)
 end
 
 
 -- In-game Date Time
 local function getInGameDateTime()
     local gameTime = getGameTime()
-	
-    if not gameTime then return end
-	
 	local year = gameTime:getYear()
 	local month = gameTime:getMonth() + 1
 	local day = gameTime:getDay() + 1
@@ -30,9 +24,61 @@ local function getInGameDateTime()
 end
 
 
--- Start Print
-if not isServer() then return end
+-- In-game Weather
+local function getInGameWeather()
+    local climateInstance = ClimateManager.getInstance()
+    local isSnowing = climateInstance:isSnowing()
+    local isRaining = climateInstance:isRaining()
+	local rainIntensity = climateInstance:getRainIntensity()
+	local cloudIntensity = climateInstance:getCloudIntensity()
+	local fogIntensity = climateInstance:getFogIntensity()
+	local windSpeed = climateInstance:getWindspeedKph()
+	local temp = climateInstance:getTemperature()
 
+    -- Weather
+	local condition = "Clear"
+    if isSnowing then
+        if rainIntensity > 0.6 then
+            condition = "Blizzard"
+        else
+            condition = "Snowing"
+        end
+    elseif isRaining then
+        if rainIntensity > 0.7 then
+            condition = "Heavy Rain"
+        elseif rain > 0.3 then
+            condition = "Rain"
+        else
+            condition = "Light Rain"
+        end
+    elseif fogIntensity > 0.5 then
+        condition = "Heavy Fog"
+    elseif fogIntensity > 0.2 then
+        condition = "Foggy"
+    elseif cloudIntensity > 0.6 then
+        condition = "Overcast"
+    elseif cloudIntensity > 0.3 then
+        condition = "Cloudy"
+    end
+
+    -- Wind Type
+    local windDesc = ""
+    if windSpeed > 40 then
+        windDesc = "(Strong Wind)"
+    elseif windSpeed > 20 then
+        windDesc = "(Windy)"
+    end
+
+    return string.format(
+        "%s %s | %.1f°C",
+        condition,
+        windDesc,
+        temp
+    )
+end
+
+
+-- Start Print
 local lastPrintTime = os.time()
 local interval = 10
 
@@ -44,15 +90,11 @@ Events.OnTick.Add(function()
 	
 	lastPrintTime = now
 
-	local ageDays = getWorldAgeDays()
-	local inGameDT = getInGameDateTime()
-
-	if not ageDays then
-		print("[MBGWorldInfo] Error: Game still loading")
-	end
+	local worldAge = getWorldAgeDays()
+	local dateTime = getInGameDateTime()
+	local weather = getInGameWeather()
 	
-	ageDays = math.floor(ageDays)
-	
-	print(string.format("[MBGWorldInfo] World Age: %d days", ageDays))
-	print(string.format("[MBGWorldInfo] Date Time: %s", inGameDT))
+	print(string.format("[MBGWorldInfo] World Age: %d days", worldAge))
+	print(string.format("[MBGWorldInfo] Date Time: %s", dateTime))
+	print(string.format("[MBGWorldInfo] Weather: %s", weather))
 end)
