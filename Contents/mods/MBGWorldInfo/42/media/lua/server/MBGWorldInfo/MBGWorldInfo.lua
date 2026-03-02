d29f3c0941bf9fec3b941b0f2b3d44bc1ebcb992
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-03-03 05:33:17 +0800

    Make code more readable.

--- Contents/mods/MBGWorldInfo/42/media/lua/server/MBGWorldInfo/MBGWorldInfo.lua
+++ Contents/mods/MBGWorldInfo/42/media/lua/server/MBGWorldInfo/MBGWorldInfo.lua
@@ -1,65 +1,58 @@
 print("[MBGWorldInfo] loaded")
 
+
 -- World Age
 local function getWorldAgeDays()
-    if getGameTime() then
-        local ageHours = getGameTime():getWorldAgeHours()
-        return ageHours / 24
-    end
-    return nil
+	local gameTime = getGameTime()
+	
+    if not gameTime then return end
+	
+	local ageHours = gameTime:getWorldAgeHours()
+	local ageDays = ageHours / 24
+	
+	return ageDays
 end
+
 
 -- In-game Date Time
 local function getInGameDateTime()
-    local gt = getGameTime()
-    if gt then
-        local year  = gt:getYear()
-        local month = gt:getMonth() + 1
-        local day   = gt:getDay() + 1
-        local hour  = gt:getHour()
-        local minute = gt:getMinutes()
-        return string.format("%04d-%02d-%02d %02d:%02d", year, month, day, hour, minute)
-    end
-    return "N/A"
+    local gameTime = getGameTime()
+	
+    if not gameTime then return end
+	
+	local year = gameTime:getYear()
+	local month = gameTime:getMonth() + 1
+	local day = gameTime:getDay() + 1
+	local hour = gameTime:getHour()
+	local minute = gameTime:getMinutes()
+	
+	return string.format("%04d-%02d-%02d %02d:%02d", year, month, day, hour, minute)
 end
+
 
 -- Start Print
-if isServer() then
-    local lastPrintTime = os.time()
-    local interval = 10 -- seconds real-life
+if not isServer() then return end
 
-    -- Print once on server start
-    Events.OnServerStarted.Add(function()
-        local ageDays = getWorldAgeDays()
-        local inGameDT = getInGameDateTime()
+local lastPrintTime = os.time()
+local interval = 10
 
-        if ageDays then
-            ageDays = math.floor(ageDays)
-            print(string.format("[MBGWorldInfo] World Age: %d days", ageDays))
-            print(string.format("[MBGWorldInfo] Date Time: %s", inGameDT))
-        else
-            print("[MBGWorldInfo] Error: Game still loading")
-        end
+-- Print Interval
+Events.OnTick.Add(function()
+	local now = os.time()
+	
+	if now - lastPrintTime < interval then return end
+	
+	lastPrintTime = now
 
-        lastPrintTime = os.time()
-    end)
+	local ageDays = getWorldAgeDays()
+	local inGameDT = getInGameDateTime()
 
-    -- Print Interval
-    Events.OnTick.Add(function()
-        local now = os.time()
-        if now - lastPrintTime >= interval then
-            lastPrintTime = now
-
-            local ageDays = getWorldAgeDays()
-            local inGameDT = getInGameDateTime()
-
-            if ageDays then
-                ageDays = math.floor(ageDays)
-                print(string.format("[MBGWorldInfo] World Age: %d days", ageDays))
-                print(string.format("[MBGWorldInfo] Date Time: %s", inGameDT))
-            else
-                print("[MBGWorldInfo] Error: Game still loading")
-            end
-        end
-    end)
-end
+	if not ageDays then
+		print("[MBGWorldInfo] Error: Game still loading")
+	end
+	
+	ageDays = math.floor(ageDays)
+	
+	print(string.format("[MBGWorldInfo] World Age: %d days", ageDays))
+	print(string.format("[MBGWorldInfo] Date Time: %s", inGameDT))
+end)
